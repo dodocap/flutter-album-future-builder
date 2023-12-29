dc32d0e3d53e7a662c90514072e4fe627128ef87
dodocap <dodocap@naver.com>
2023-12-29 15:58:27 +0900

    refactor: 뷰모델 수정

--- lib/ui/main/main_screen.dart
+++ lib/ui/main/main_screen.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:orm_album_future_builder/model/album.dart';
-import 'package:orm_album_future_builder/repository/album_repository.dart';
-import 'package:orm_album_future_builder/repository/album_repository_impl.dart';
 import 'package:orm_album_future_builder/ui/main/main_view_model.dart';
 
-class MainScreen extends StatelessWidget {
-  final AlbumRepository albumRepository = AlbumRepositoryImpl();
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
 
-  MainScreen({super.key});
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  final MainViewModel _mainViewModel = MainViewModel();
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +20,7 @@ class MainScreen extends StatelessWidget {
         title: const Text('앨범 리스트'),
       ),
       body: FutureBuilder<List<Album>>(
-        future: albumRepository.getAlbums(),
+        future: _mainViewModel.getAlbums(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
@@ -26,22 +29,21 @@ class MainScreen extends StatelessWidget {
             return Center(child: Text(snapshot.error.toString()));
           }
 
-          final List<Album> albumList = (snapshot.data!)..removeWhere((element) => element.id == -1);
+          final List<Album> albumList = snapshot.data!;
           return ListView.builder(
             itemCount: albumList.length,
             itemBuilder: (context, index) {
               final Album album = albumList[index];
-              return MainViewModel(
-                album: album,
-                onAlbumPressed: (id, title) {
-                  context.push(Uri(
-                    path: '/detail',
-                    queryParameters: {
-                      'id': id,
-                      'title': title
-                    },
-                  ).toString());
-                },
+              return ListTile(
+                title: Text(album.title),
+                onTap: () => context.push(Uri(
+                  path: '/detail',
+                  queryParameters: {
+                    'id': album.id.toString(),
+                    'title': album.title
+                  },
+                ).toString()),
+                trailing: const Icon(Icons.navigate_next),
               );
             },
           );
--- lib/ui/main/main_view_model.dart
+++ lib/ui/main/main_view_model.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:orm_album_future_builder/model/album.dart';
+import 'package:orm_album_future_builder/repository/album_repository.dart';
+import 'package:orm_album_future_builder/repository/album_repository_impl.dart';
 
-class MainViewModel extends StatelessWidget {
-  final Album _album;
-  final void Function(String id, String title) onAlbumPressed;
+class MainViewModel {
+  final AlbumRepository _albumRepository = AlbumRepositoryImpl();
 
-  const MainViewModel({
-    super.key,
-    required Album album,
-    required this.onAlbumPressed,
-  }) : _album = album;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(_album.title),
-      onTap: () => onAlbumPressed(_album.id.toString(), _album.title),
-      trailing: const Icon(Icons.navigate_next),
-    );
+  Future<List<Album>> getAlbums() async {
+    final List<Album> albumList = await _albumRepository.getAlbums();
+    albumList.removeWhere((element) => element.id == -1);
+    return albumList;
   }
 }
